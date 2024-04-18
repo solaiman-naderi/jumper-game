6568bfd93a4f0292522916b640f15e1c6ffcd185
solaiman-naderi <solaimannaderi00@gmail.com>
2024-04-18 14:47:23 +0330

    handle logic in useGameState for start game

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { Experience } from "./components/Experience";
 
 import { UI } from "./components/UI";
 import { AudioManagerProvider } from "./hooks/useAudioManager";
-
+import { GameStateProvider } from "./hooks/useGameState";
 function App() {
   return (
     <AudioManagerProvider>
-      <Canvas shadows camera={{ position: [0, 16, 10], fov: 42 }}>
-        <color attach="background" args={["#041c0b"]} />
-        <Experience />
-      </Canvas>
-      <UI />
+      <GameStateProvider>
+        <Canvas shadows camera={{ position: [0, 16, 10], fov: 42 }}>
+          <color attach="background" args={["#041c0b"]} />
+          <Experience />
+        </Canvas>
+        <UI />
+      </GameStateProvider>
     </AudioManagerProvider>
   );
 }
--- src/hooks/useGameState.jsx
+++ src/hooks/useGameState.jsx
@@ -1,5 +1,10 @@
-import { isHost, useMultiplayerState } from "playroomkit";
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  Joystick,
+  isHost,
+  onPlayerJoin,
+  useMultiplayerState,
+} from "playroomkit";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 
 const GameStateContext = createContext();
 
@@ -12,7 +17,7 @@ const NEXT_STAGE = {
 
 const TIMER_STAGE = { lobby: -1, countdown: 3, game: 0, winner: 5 };
 
-export const gameStateProvider = ({ children }) => {
+export const GameStateProvider = ({ children }) => {
   const [stage, setStage] = useMultiplayerState("gameStage", "lobby");
   const [timer, setTimer] = useMultiplayerState("timer", TIMER_STAGE.lobby);
   const [players, setPlayers] = useState([]);
@@ -20,8 +25,62 @@ export const gameStateProvider = ({ children }) => {
 
   const host = isHost();
 
+  const isInit = useRef(false);
+  useEffect(() => {
+    if (isInit.current) {
+      return;
+    }
+    isInit.current = true;
+    onPlayerJoin((state) => {
+      const controls = new Joystick(state, {
+        type: "angular",
+        buttons: [{ id: "jump", label: "jump" }],
+      });
+      const newPlayer = { state, controls };
+
+      setPlayers((players) => [...players, newPlayer]);
+
+      state.onQuit(() => {
+        setPlayers((players) => players.filter((p) => p.state.id !== stage.id));
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!host) return;
+    if (stage === "lobby") return;
+    const timeout = setTimeout(() => {
+      let newTime = stage === "game" ? timer + 1 : timer - 1;
+      if (newTime === 0) {
+        const nextStage = NEXT_STAGE[stage];
+        setStage(nextStage, true);
+        newTime = TIMER_STAGE[nextStage];
+      }
+      setTimer(newTime, true);
+      return () => clearTimeout(timeout);
+    }, 1000);
+  }, [host, timer, stage, soloGame]);
+
+  const startGame = () => {
+    setStage("countdown");
+    setTimer(TIMER_STAGE.countdown);
+    if (players.length === 1) {
+      setSoloGame(true);
+    }
+  };
+
   return (
-    <GameStateContext.Provider value={{}}>{children}</GameStateContext.Provider>
+    <GameStateContext.Provider
+      value={{
+        stage,
+        timer,
+        players,
+        host,
+        startGame,
+      }}
+    >
+      {children}
+    </GameStateContext.Provider>
   );
 };
 
